f32d838ce30064586769890407b295fbddde5f2c
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-20 23:53:32 +0200

    day1-3[input exercise]

---
+++ day-1/print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Practicing python
+with Angela Yu's course
+"""
+
+
+a = input("Hello! What's your stat?\n")
+b = len(a)
+print(b)
+print("----or---\n")
+print(len(input("What's your name? ")))
