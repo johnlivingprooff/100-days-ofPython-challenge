59b9a2f4533f8929cd14e67b64e81529bce01a83
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-21 00:15:15 +0200

    final-project Day-1

---
+++ day-1/final_project.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# A Band Name Generator
+print("Welcome to the Band Name Generator.")
+cityName = input("What's the City you grew up in?\n")
+petName = input("What's your pets name?\n")
+print("Your band name could be " + cityName + " " + petName)
+print()
